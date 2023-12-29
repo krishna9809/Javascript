563533265c457c6c0ed5a9d50ccc052e14b8b825
krishna9809 <multiatoms980996@gmail.com>
2023-12-29 19:23:31 +0530

    Day01 with JS

---
+++ Day-001/script.js
@@ -0,0 +1,5 @@
+const accountId = 144553;
+let accountEmail = "krish@gmail.com";
+var accountPass = "3456";
+accountCity = "Agra";
+console.table([accountId, accountEmail, accountPass, accountCity]);
