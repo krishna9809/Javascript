c021de7d942794107b7459edea2fc0010d15b82e
krishna9809 <multiatoms980996@gmail.com>
2023-12-30 20:49:28 +0530

    Day 2 with JS

---
+++ Day-002/Code/script.js
@@ -0,0 +1,29 @@
+// Data Type
+
+
+"use strict" // treat as newer version of JS
+
+// alert(3+3)
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+
+
+// Datatype
+
+// 1. Number
+let no = 1;
+// 2. BigINt
+let bigint = 2222222222222222222222222222222;
+// 3. Boolean
+let bool = true;
+// 4.  String 
+let name = "krishna";
+//5. null
+let hell = null;
+// 6. undefined;
+let uni = undefined
+// 7. Symbol => Unique
+
+console.table([typeof(no), typeof(bigint), typeof(bool) ,typeof(name), typeof(hell), typeof(uni)]);
