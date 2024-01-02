32513b38e2431cde97da2145cbe724979bdba755
krishna9809 <multiatoms980996@gmail.com>
2024-01-02 19:09:22 +0530

    Day-005 with JS

---
+++ Day-005/script.js
@@ -0,0 +1,24 @@
+// Comparision
+
+console.log(2>3); // False
+console.log(2==2); // True
+console.log(2>=3); // False
+console.log(3!=2); //Ture
+console.log(2<=3); //True
+
+
+
+//Diffrent DataType
+
+console.log("2" > 1); //True
+console.log(null == 0) // False
+console.log(null >= 0); // True
+console.log(undefined == 0) // False
+
+
+
+
+// Two types of Datatypes 
+
+// 1. Primitive - String , Number , BigInt , null , undefined , Boolean , Symbol
+// 2. Non-Primitive - Object , Array , Function , 
