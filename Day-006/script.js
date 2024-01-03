6f3043543dfdc2e7fc3cfa1da7537fcd1a910d50
krishna9809 <multiatoms980996@gmail.com>
2024-01-03 20:12:47 +0530

    Day06 with JS

---
+++ Day-006/script.js
@@ -0,0 +1,31 @@
+// Stack (Primitive) 
+// Heap (Non-Primitive)
+
+ 
+
+let user = "Krishna";
+
+let userTwo = user;
+
+console.log(userTwo)
+
+
+userTwo = "Jitesh";
+console.log(userTwo)
+console.log(user)
+
+
+
+let obj =  {
+    name : "Krishna",
+    Roll_no : 3
+
+}
+
+let obj2 = obj;
+
+console.log(obj2)
+
+obj2.name = "Jitesh";
+
+console.log(obj)
