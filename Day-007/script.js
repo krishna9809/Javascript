7c92c405bcb3eb6976efb7c853250b6d422d79f8
krishna9809 <multiatoms980996@gmail.com>
2024-01-04 19:41:27 +0530

    Day-07 with JS

---
+++ Day-007/script.js
@@ -0,0 +1,55 @@
+// String 
+
+
+let name = "krishna"
+
+console.log(`Your name is ${name}`);
+
+kk = new String('KrihsnaSharma');
+
+console.log(kk)
+
+
+
+
+// String Methods 
+
+// String length
+// String charAt()
+// String charCodeAt()
+// String at()
+// String [ ]
+// String slice()
+// String substring()
+// String substr()
+
+
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
+
+
+let sampleString = "String is Cool";
+
+console.log(sampleString.slice(0,4));
+
+console.log(sampleString.charCodeAt(0))
+
+console.log(sampleString.at(0))
+
+console.log(sampleString.substring(0,8))
+
+console.log(sampleString.toLocaleLowerCase());
+
+console.log(sampleString.toUpperCase());
+
+
