15663b2dafd5d3eb13209c79d21835068271c9ef
krishna9809 <multiatoms980996@gmail.com>
2024-01-05 21:54:43 +0530

    Math and Num

---
+++ Day-008/script.js
@@ -0,0 +1,24 @@
+const score = 400
+
+const balance = new Number(100);
+console.log(score) // 400
+console.log(balance) // [Number: 100]
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+let num = 123.4563;
+
+console.log(num.toFixed(2))
+
+console.log(num.toPrecision(4))
+console.log(num.toPrecision(3))
+
+
+let bigNum = 100000000000;
+
+console.log(bigNum.toLocaleString('en-IN'))
+
+
+
