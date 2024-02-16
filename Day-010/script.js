bd43228473aa463ed10b286ee93d231ce70e661a
krishna9809 <multiatoms980996@gmail.com>
2024-02-16 23:01:37 +0530

    array day2

--- Day-010/script.js
+++ Day-010/script.js
@@ -8,3 +8,27 @@ console.log(array)
 array.pop()
 
 console.log(array)
+
+let superHero = ["hero", "zero"];
+
+console.log(superHero)
+
+array.shift()
+
+console.log(array)
+
+array.unshift(3)
+
+console.log(array);
+
+
+let marvel = ["ironman", "Hulk", "Thor"];
+let DC = ["superman", "batman"];
+
+// marvel.push(DC);
+
+let MarvelDC = marvel.concat(DC)
+
+console.log(marvel);
+
+console.log(MarvelDC)
